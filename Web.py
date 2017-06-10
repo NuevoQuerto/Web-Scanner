3fb9252bb23dc7dfdec385a2d7658db14432269a
Agung Rizal <NuevoQuerto@gmail.com>
2017-06-10 12:24:04 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -7,7 +7,7 @@ print """
 \t\t\t|          devilzc0de.org           |
 \t\t\t|     We Are Coder And Exploiter    |
 \t\t\t|                                   |
-\t\t\t|  Web Scanner Project Version 0.2  |
+\t\t\t|  Web Scanner Project Version 0.3  |
 \t\t\t|     Created By Nuevo Querto       |
 \t\t\t|-----------------------------------|
 """
@@ -15,11 +15,12 @@ print """
 try:
 	Parser = argparse.ArgumentParser(prog="Web.py", description="Untuk Scanning Website")
 
-	Parser.add_argument("-u", "--url", help="Target URL, Example: http://www.target.com?id=1", action="store", default=False, dest="URL")
+	Parser.add_argument("-u", "--url", help="Target Wordlists, Example: User/Target.txt", action="store", default=False, dest="Target")
 	Parser.add_argument("--wordlist", help="Wordlists File, Example: User/wordlist.txt", action="store", default=False, dest="Wordlist")
 
 	Args = Parser.parse_args()
-	Checking_Parameter = re.search(r"\?|=", Args.URL, re.M|re.I)
+	
+	TargetWordlists = []
 	
 	XSS_Payload = []
 	
@@ -37,22 +38,29 @@ try:
 	else:
 		for x in range(len(Default_Payload)):
 			Import_Payload = XSS_Payload.append("%s" %(Default_Payload[x]))
-	
-	if Checking_Parameter:
-		print "\n\n\n"
 		
-		for i in range(len(XSS_Payload)):
-			InjectingPayload = Args.URL + "%s" %(XSS_Payload[i]) # Injecting XSS Payload
-			Request = requests.get(InjectingPayload)
-			HTTPCode = Request.status_code
-			Checking_Payload = re.search(re.escape(r"%s" %(XSS_Payload[i])), Request.text, re.M|re.I)
-			
-			print "Payload: " + InjectingPayload + " %s" %("[ Vuln To XSS ] [ %s ]\n" if Checking_Payload else "[ Not Vuln To XSS ] [ %s ]\n") %(HTTPCode)
+	if Args.Target:
+		with open(Args.Target, "r+") as file:
+			Read = file.read()
+			Pisahkan = Read.split("\n")
+		for i in range(len(Pisahkan)):
+			Import_Target = TargetWordlists.append("%s" %(Pisahkan[i]))
+			Checking_Parameter = re.search(r"\?|=", TargetWordlists[i], re.M|re.I)
+			print "\n\n\n" + "[ Target %s: " %(i + 1) + TargetWordlists[i] + " ]\n"
+			if Checking_Parameter:
+				for z in range(len(XSS_Payload)):
+					InjectingPayload = TargetWordlists[i] + "%s" %(XSS_Payload[z]) # Injecting XSS Payload
+					Request = requests.get(InjectingPayload)
+					HTTPCode = Request.status_code
+					Checking_Payload = re.search(re.escape(r"%s" %(XSS_Payload[z])), Request.text, re.M|re.I)
+					
+					print "Payload: " + InjectingPayload + " %s" %("[ Vuln To XSS ] [ %s ]\n" if Checking_Payload else "[ Not Vuln To XSS ] [ %s ]\n") %(HTTPCode)
+			else:
+				print TargetWordlists[i] + " -> Not Valid Target URL, Example http://target.com?id=1\n"
+			print "[ -- ]"
 	else:
-		print "Masukkan Target Dengan Benar, Example: http://www.target.com?id=1"
+		print "Use -h or --help for Help"
 except IOError:
 	print "File Tidak Ditemukan"
-except TypeError:
-	print "Use -h or --help For Help"
 except KeyboardInterrupt:
 	print "User Cancelling"
